33a3f620619792aba3dcabeb49a6a27591808425
Alex Wan <wanxudong@ezxr.com>
2021-12-08 16:26:31 +0800

    (feat):原生代码

---
+++ rpg_vikit/vikit_common/build/CMakeFiles/test_vk_common_camera.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/ros/svo_ws/src/rpg_vikit/vikit_common/test/test_camera.cpp" "/home/alex/ros/svo_ws/src/rpg_vikit/vikit_common/build/CMakeFiles/test_vk_common_camera.dir/test/test_camera.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"vikit_common\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/eigen3"
+  "/home/alex/ros/svo_ws/src/Sophus"
+  "/usr/include/eigen3"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/alex/ros/svo_ws/src/rpg_vikit/vikit_common/build/CMakeFiles/vikit_common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rpg_vikit/vikit_common/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/alex/ros/svo_ws/src/rpg_vikit/vikit_common/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
